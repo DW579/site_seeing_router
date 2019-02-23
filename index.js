3e2e80990fab6d184e7188b47de231086ca4d058
Dustin Wurtz <dtwurtz@gmail.com>
2019-02-23 14:27:40 -0800

    Clarifai api hooked up

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const Clarifai = require('clarifai');
+const port = 3000;
+const clarApp = new Clarifai.App({
+    apiKey: process.env.CARIFAI_API
+   });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    console.log(clarApp);
+    res.send(clarApp);
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
