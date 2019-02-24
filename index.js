3d718297a64f09a7fb732a45d275be4019336edc
Dustin Wurtz <dtwurtz@gmail.com>
2019-02-23 17:18:55 -0800

    Trained model and predicting

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const port = 3000;
 const clarApp = new Clarifai.App({
     apiKey: process.env.CARIFAI_API
    });
+const image = "";
 
 app.get('/', (req, res) => {
-    console.log(clarApp);
-    res.send(clarApp);
+    clarApp.models.predict({id: 'SF_Landmarks', version: process.env.MODEL_VERSION}, {base64: image}).then(
+        function(response) {
+            console.log(response.outputs[0].data.concepts);
+        },
+        function(err) {
+          console.log(err);
+        }
+    );
+
+    res.send("Main page");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
